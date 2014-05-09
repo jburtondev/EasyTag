ad1d9cb8984aecbba365ea8762ddd5a2fb05e039
Jamie Burton <jamiejburton@hotmail.co.uk>
2014-05-09 17:22:28 +0100

    Update main.js
    Enabled multiple entries to export

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
+//Sets the textbox height and width
 var box_big = document.getElementById("p_tag_export").style.width = 276 + "px";
+document.getElementById("p_tag_export").style.height = 276 + "px";
 
-
-		
 //Create an empty array to store the users input data
 tagList = [];
 
 //Variable containing the first input box
-var p = document.getElementById("p_tag1");
+var p = document.getElementById("newInput0");
 
 //Variable containing error message for 'export_p_tag()' function
 var error_message = "Es funktioniert nicht!";
 
 //Value of the '<p></p>' character length
 var tag_val = 7;
+
+var countElement = 1;
 
 //Variable containing the new element generated from 'createInputElement()'
 var element;
@@ -27,23 +29,19 @@ function insertAfter(referenceNode, newNode) {
 function createInputElement() {
     element = document.createElement("input"); //Creates input element
     element.setAttribute("type", "text"); //Sets the element 'type = text'
-    element.setAttribute("id", "newInput"); //Sets the element 'id = newInput'
+    element.setAttribute("id", "newInput" + countElement); //Sets the element 'id = newInput'
+    element.setAttribute("class", "latestfield");
     element.setAttribute("placeholder", "Enter more text here"); //Sets the element 'placeholder = Enter more text here'
     document.body.appendChild(element); //Appends new element to the body
     console.log("<input> element created");
-
+    countElement++;
     //Variable containing the new element
     var i = document.getElementById("newInput"); //Variable containing the new element
 
     insertAfter(p, element); //See function 'insertAfter'
     console.log("Inserted after p 'Enter text here'");
-    //Increases the style width of the 'big_box' class
-	
-//Fix this button
-	if (i) {
-		var change = document.getElementById("p_tag_btn").innerHTML = "Remove button";
-	} 
-	
+    //Increases the style width of the 'big_box' class 
+
 }
 
 //Function that exports the <input> text within tags'<p>UsersText</p>
@@ -56,15 +54,18 @@ Tag_Exporter = {
 		Runs conditional logic: 
 		...This sequence is documented in the below code
 		1) Checks if the 'createInputElement()' function has created a new element
-		2) If a new <input> element has not been created, export '<p>UsersText</p>'
+		2) If a new <input> element has not been created, export '<tag>UsersText</tag>'
 		3) If the new <input> element has been created then export both ' tagList[0] ' & ' tagList[1]'	
 	*/
 
         // 1) Checks if the 'createInputElement()' function has created a new element
         if (typeof element === 'undefined') {
             // 2) If a new <input> element has not been created, export '<p>UsersText</p>'
-			var p_export = document.getElementById("p_tag_export").innerHTML = p_export = "<"+document.getElementById("selectTag").value+">" + p.value + "</"+document.getElementById("selectTag").value+">";
-			
+
+            //Selects the '<select>' id and gets the value
+            //Uses this value to then 
+            var p_export = document.getElementById("p_tag_export").innerHTML = p_export = "<" + document.getElementById("selectTag").value + ">" + p.value + "</" + document.getElementById("selectTag").value + ">";
+
             p.style.color = "green";
 
             if (p_export == "<p></p>") {
@@ -75,20 +76,27 @@ Tag_Exporter = {
             }
 
         } else {
-			
-	var newField = document.getElementById("newInput").innerHTML = element.value; //Variable containing the new element
-            tagList.push(p.value, newField); // Stores data pushed from p_value 
-            tagList.toString(); //Converts the tagList array to a string
-            //3) If the new <input> element has been created then export both ' tagList[0] ' & ' tagList[1]'
 
-            var p_export = document.getElementById("p_tag_export").innerHTML = "<"+document.getElementById("selectTag").value+">" + tagList[0] + "</"+document.getElementById("selectTag").value+">" + '\n' + "<"+document.getElementById("selectTag").value+">" + tagList[1] + "</"+document.getElementById("selectTag").value+">";
-				
-        } 
+            var p_export = document.getElementById("p_tag_export").innerHTML = exportArray();
 
-        console.log(p_export.length);
+        }
 
     }
 }
+
+//Iterates through the countElement
+function exportArray() {
+
+    var j = "";
+    for (var i = 0; i < countElement; i++) {
+        if (document.getElementById("newInput" + i).value != "") {
+            j = j + "<" + document.getElementById("selectTag").value + ">" + document.getElementById("newInput" + i).value + "</" + document.getElementById("selectTag").value + ">" + "\n";
+
+        }
+    }
+    return j;
+}
+
 
 //Inherits properties from 'Tag_Exporter', specifically the 'ConvertTag' function
 Paragraph_Tag_Exporter = Object.create(Tag_Exporter);
