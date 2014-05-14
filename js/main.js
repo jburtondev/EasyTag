f50ae90b82ba71bfa18c7ae43437435dbd645bc8
Jamie Burton <jamiejburton@hotmail.co.uk>
2014-05-14 15:36:22 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
-//Sets the textbox height and width
+//------------------------------------Global space---------------------------------------------//
+
+//Sets the main <textbox> height and width
 var box_big = document.getElementById("p_tag_export").style.width = 276 + "px";
 document.getElementById("p_tag_export").style.height = 276 + "px";
-
-//Create an empty array to store the users input data
-tagList = [];
 
 //Variable containing the first input box
 var p = document.getElementById("newInput0");
@@ -24,8 +23,12 @@ function insertAfter(referenceNode, newNode) {
     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 
+//------------------------------------ End of Global space---------------------------------------------//
 
-//Function thats creates  a new '<input>' element
+
+
+//-----------------------------------Add Field Button Function-----------------------------------//
+
 function createInputElement() {
     element = document.createElement("input"); //Creates input element
     element.setAttribute("type", "text"); //Sets the element 'type = text'
@@ -44,19 +47,22 @@ function createInputElement() {
 
 }
 
-//Function that exports the <input> text within tags'<p>UsersText</p>
+
 Tag_Exporter = {
+
+    //-----------------------------------'Convert Tag' Button Function-----------------------------------//
+
     ConvertTag: function () {
 
         p.style.color = "black"; //Initialise/Reinitialise the input color to black
-        
-	/*	
-	Runs conditional logic: 
-	...This sequence is documented in the below code
-	1) Checks if the 'createInputElement()' function has created a new element
-	2) If a new <input> element has not been created, export 'selectTag.value'
-	3) If the new <input> element has been created then export both 'selectTag.value' & 'selectTag.value'		
-	*/
+
+        /*	
+			Runs conditional logic: 
+			...This sequence is documented in the below code
+			1) Checks if the 'createInputElement()' function has created a new element
+			2) If a new <input> element has not been created, export '<tag>UsersText</tag>'
+			3) If the new <input> element has been created then export both 'selectTag.value' & 'selectTag.value'	
+			*/
 
         // 1) Checks if the 'createInputElement()' function has created a new element
         if (typeof element === 'undefined') {
@@ -70,7 +76,7 @@ Tag_Exporter = {
 
             if (p_export == "<p></p>") {
                 console.log("User submitted empty form");
-                p.value = "Empty form submitted...";
+                p.value = "Empty form page..";
                 p.style.color = "red";
 
             }
@@ -82,38 +88,54 @@ Tag_Exporter = {
         }
 
     }
-}
 
+    //-----------------------------------End of 'Convert Tag Button' Function-----------------------------------//
 
+};
+
+//-----------------------------------Export Array Function-----------------------------------//
 
 //Iterates through the countElement
 function exportArray() {
 
     var j = "";
     for (var i = 0; i < countElement; i++) {
-        if (document.getElementById("newInput" + i).value != "") {
+        if (document.getElementById("newInput" + i).value !== "") {
+
+            //Create an empty array to store the users input data
+            var tagList = [];
+
             var p = document.getElementById("newInput" + i).value;
-            tagList.push(p)
+            taglist = tagList.push(p);
 
-
-            //Sorting algorithm function
+            //Perfom sorting algorithm function
             tagList.sort(
+
             function (a, b) {
                 return a - b;
             });
 
-            console.log("Pushed newInput[] into array");
+            tagList.length = 0; //Empty array to prevent duplicate values
+            
+            console.log("Pushed newInput[] into array " + document.getElementById("selectTag").value);
             j = j + "<" + document.getElementById("selectTag").value + ">" + document.getElementById("newInput" + i).value + "</" + document.getElementById("selectTag").value + ">" + "\n";
 
         }
     }
+    
+
+
     return j;
 }
 
+//-----------------------------------End of Export Array Function-----------------------------------//
 
 //Inherits properties from 'Tag_Exporter', specifically the 'ConvertTag' function
 Paragraph_Tag_Exporter = Object.create(Tag_Exporter);
 //Paragraph_Tag_Exporter.ConvertTag() will convert the user input into a HTML paragraph tag
+
+
+//-----------------------------------Take Tag Button Function - *Needs fixing*-----------------------------------//
 function take_tag() {
     make_box = document.createElement("textbox"); //Creates <input> element
     make_box.setAttribute("id", "take_tag");
@@ -121,3 +143,4 @@ function take_tag() {
     make_box_text = document.getElementById("take_tag");
     document.body.appendChild(make_box);
 }
+//-----------------------------------End of 'Take Tag' Button Function - *Needs fixing*-----------------------------------//
