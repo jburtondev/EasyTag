b0794f039538c1f7c0fbfd7dcd2408854df905b2
Jamie Burton <jamiejburton@hotmail.co.uk>
2014-09-23 14:02:03 +0100

    Cleaned up code

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 //------------------------------------Global space---------------------------------------------//
-
 //Variable containing the first <select> element on the page
 var select_elem = document.getElementById("selectTag0");
 
@@ -18,51 +17,44 @@ var count_element = 1;
 var element;
 
 //Works with the 'createInputElement()' function to insert after the current <input> field
+
 function insertAfter(referenceNode, newNode) {
-    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+   referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 
 }
 
 //------------------------------------ End of Global space---------------------------------------------//
-
-
-
 //-----------------------------------Add Field Button Function-----------------------------------//
 
 function createInputElement() {
-    var select_elem_clone = select_elem.cloneNode(true);
-    select_elem_clone.setAttribute("id", "selectTag" + count_element);
-    select_elem_clone.setAttribute("style", "float:left;");
-    document.body.appendChild(select_elem_clone);
-    insertAfter(p, select_elem_clone); //See function 'insertAfter'
-
-    element = document.createElement("input"); //Creates input element
-    element.setAttribute("type", "text"); //Sets the element 'type = text'
-    element.setAttribute("id", "newInput" + count_element); //Sets the element 'id = newInput'
-    element.setAttribute("class", "latestfield");
-    element.setAttribute("placeholder", "Enter more text here"); //Sets the element 'placeholder = Enter more text here'
-    document.body.appendChild(element); //Appends new element to the body
-    console.log("<input> element created");
-    count_element++;
-    //Variable containing the new element
-    var i = document.getElementById("newInput"); //Variable containing the new element
-
-    insertAfter(p, element); //See function 'insertAfter'
-    console.log("Inserted after p 'Enter text here'");
-    //Increases the style width of the 'big_box' class 
-
+   var select_elem_clone = select_elem.cloneNode(true);
+   select_elem_clone.setAttribute("id", "selectTag" + count_element);
+   select_elem_clone.setAttribute("style", "float:left;");
+   document.body.appendChild(select_elem_clone);
+   insertAfter(p, select_elem_clone); //See function 'insertAfter'
+   element = document.createElement("input"); //Creates input element
+   element.setAttribute("type", "text"); //Sets the element 'type = text'
+   element.setAttribute("id", "newInput" + count_element); //Sets the element 'id = newInput'
+   element.setAttribute("class", "latestfield");
+   element.setAttribute("placeholder", "Enter more text here"); //Sets the element 'placeholder = Enter more text here'
+   document.body.appendChild(element); //Appends new element to the body
+   console.log("<input> element created");
+   count_element++;
+   //Variable containing the new element
+   var i = document.getElementById("newInput"); //Variable containing the new element
+   insertAfter(p, element); //See function 'insertAfter'
+   console.log("Inserted after p 'Enter text here'");
+   //Increases the style width of the 'big_box' class 
 }
 
 
 Tag_Exporter = {
 
-    //-----------------------------------'Convert Tag' Button Function-----------------------------------//
+   //-----------------------------------'Convert Tag' Button Function-----------------------------------//
+   ConvertTag: function () {
 
-    ConvertTag: function () {
-
-        p.style.color = "black"; //Initialise/Reinitialise the input color to black
-
-        /*	
+      p.style.color = "black"; //Initialise/Reinitialise the input color to black
+/*	
 	Runs conditional logic: 
 	...This sequence is documented in the below code
 	1) Checks if the 'createInputElement()' function has created a new element
@@ -70,107 +62,97 @@ Tag_Exporter = {
 	3) If the new <input> element has been created then export both 'selectTag.value' & 'selectTag.value'	
 	*/
 
-        // 1) Checks if the 'createInputElement()' function has created a new element
-        if (typeof element === 'undefined') {
-            // 2) If a new <input> element has not been created, export '<p>UsersText</p>'
+      // 1) Checks if the 'createInputElement()' function has created a new element
+      if (typeof element === 'undefined') {
+         // 2) If a new <input> element has not been created, export '<p>UsersText</p>'
+         var p_export = document.getElementById("p_tag_export").innerHTML = p_export = "<" + document.getElementById("selectTag0").value + ">" + p.value + "</" + document.getElementById("selectTag0").value + ">";
 
-            var p_export = document.getElementById("p_tag_export").innerHTML = p_export =
-                "<" + document.getElementById("selectTag0").value + ">" + p.value +
-                "</" + document.getElementById("selectTag0").value + ">";
+         p.style.color = "green";
 
-            p.style.color = "green";
+      } else var p_export = document.getElementById("p_tag_export").innerHTML = exportArray();
 
-        } else var p_export = document.getElementById("p_tag_export").innerHTML = exportArray();
+      p.style.color = "green";
 
-        p.style.color = "green";
+      if (p_export == "<p></p>") {
+         console.log("User submitted empty form");
+         p.value = "User submitted empty form";
+         p.style.color = "red";
 
-        if (p_export == "<p></p>") {
-            console.log("User submitted empty form");
-            p.value = "User submitted empty form";
-            p.style.color = "red";
+      }
 
-        }
-
-    }
+   }
 
 };
 
 //-----------------------------------End of 'Convert Tag Button' Function-----------------------------------//
-
-
-
 //-----------------------------------Export Array Function-----------------------------------//
-
 //Iterates through the count_element
+
 function exportArray() {
 
-    var j = "";
-    for (var i = 0; i < count_element; i++) {
-        if (document.getElementById("newInput" + i).value !== "" && document.getElementById("newInput" + i).value !== "") {
+   var j = "";
+   for (var i = 0; i < count_element; i++) {
+      if (document.getElementById("newInput" + i).value !== "" && document.getElementById("newInput" + i).value !== "") {
 
-            //Create an empty array to store the users input data
-            var tagList = [];
+         //Create an empty array to store the users input data
+         var tagList = [];
 
-            var p = document.getElementById("newInput" + i).value;
-            taglist = tagList.push(p);
+         var p = document.getElementById("newInput" + i).value;
+         taglist = tagList.push(p);
 
-            //Perform sorting algorithm function
-            tagList.sort(
+         //Perform sorting algorithm function
+         tagList.sort(
 
-            function (a, b) {
-                return a - b;
-            });
+         function (a, b) {
+            return a - b;
+         });
 
-            tagList.length = 0; //Empty array to prevent duplicate values
+         tagList.length = 0; //Empty array to prevent duplicate values
+         console.log("Pushed newInput " + [i] + " into array " + document.getElementById("selectTag0").value);
+         j = j + "<" + document.getElementById("selectTag" + i).value + ">" + document.getElementById("newInput" + i).value + "</" + document.getElementById("selectTag" + i).value + ">" + "\n";
 
-            console.log("Pushed newInput " + [i] + " into array " + document.getElementById("selectTag0").value);
-            j = j + "<" + document.getElementById("selectTag" + i).value + ">" + document.getElementById("newInput" + i).value + "</" + document.getElementById("selectTag" + i).value + ">" + "\n";
-
-        }
-    }
-
+      }
+   }
 
 
-    return j;
+
+   return j;
 }
 
 //-----------------------------------End of Export Array Function-----------------------------------//
-
 //Inherits properties from 'Tag_Exporter', specifically the 'ConvertTag' function
 Paragraph_Tag_Exporter = Object.create(Tag_Exporter);
 //Paragraph_Tag_Exporter.ConvertTag() will convert the user input into a HTML paragraph tag
-
-
 //-----------------------------------Take Tag Button Function - *Needs fixing*-----------------------------------//
+
 function takeTag() {
 
-    if (p.value === "") {
-        alert("Nothing to output");
-    } else {
+   if (p.value === "") {
+      alert("Nothing to output");
+   } else {
 
-        var text_output = document.getElementById("p_tag_export");
+      var text_output = document.getElementById("p_tag_export");
 
-        var html_holder = document.getElementById("export_box"); //.innerHTML = text_output.value;
+      var html_holder = document.getElementById("export_box"); //.innerHTML = text_output.value;
+      html_holder.innerHTML = html_holder.innerHTML + text_output.innerHTML + "\n";
 
-        html_holder.innerHTML = html_holder.innerHTML + text_output.innerHTML + "\n";
+      text_output.value = "";
 
-        text_output.value = "";
+      console.log("Emptied main output");
 
-        console.log("Emptied main output");
+      function removeInputFields() {
+         for (i = 1; i < 15; i++) {
+            newInputFields = document.getElementById("newInput" + i);
+            form_container.removeChild(newInputFields);
+            selectTagFields = document.getElementById("selectTag" + i);
+            form_container.removeChild(selectTagFields);
 
-        function removeInputFields() {
-            for (i = 1; i < 15; i++) {
-                newInputFields = document.getElementById("newInput" + i);
-                form_container.removeChild(newInputFields);
-                selectTagFields = document.getElementById("selectTag" + i);
-                form_container.removeChild(selectTagFields);
+            resetFirstInput = document.getElementById("newInput0").value = "";
 
-                resetFirstInput = document.getElementById("newInput0").value = "";
-
-            }
-        }
-        removeInputFields();
-    }
+         }
+      }
+      removeInputFields();
+   }
 
 }
 //-----------------------------------End of 'Take Tag' Button Function - *Needs fixing*-----------------------------------//
